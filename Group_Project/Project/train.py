78404ff62c51d2bc8a4145e4cf40450c48605725
Idan Grady <63195210+idangrady@users.noreply.github.com>
2022-01-06 17:42:59 +0200

    Update train.py

--- Group_Project/Project/train.py
+++ Group_Project/Project/train.py
@@ -7,20 +7,43 @@ import os
 import torch.nn as nn
 from typing import Dict, Callable, List
 from torch import FloatTensor, LongTensor
+from Embedding import word_embedding
 
-
-def train(input_data: Floattensor,target_data:FloatTensor,  optim, criterion , embedding,num_itter, num_episode, alpha, lr):
+def train_rnn(input_data: FloatTensor,target_data:FloatTensor,  optim, criterion , embedding,num_itter, num_episode, alpha, lr):
     
     # init with zero optimizers
     optim.zero_grad()
     loss = 0
     
     for i in range(input_data.size(0)):
-        output, hidden = rnn()
-        loss+= criterion (prediction, )
         pass
     
 
-    optim = optim.Adam(model.parameters(), lr)
+    #optim = optim.Adam(model.parameters(), lr)
     
-    loss = model.
+
+
+def embedding_train(embedding: object, num_epoch: int,X:FloatTensor, X_weighted: FloatTensor,lr: FloatTensor , optimizer:object):
+    glove=embedding
+    losses = []
+
+    opt = optimizer(glove.parameters(),lr)
+    opt.zero_grad()
+    
+    for i in range(num_epoch):
+        # embedding return loss_(X_weighted, W, Wc, b, bc, X)
+        loss = glove(X_weighted)
+        # step back with gradients
+        loss.backward()
+        # update the gradients. 
+        opt.step() # compute the gradients
+        
+        opt.zero_grad()
+        # appends
+        losses.append(loss)
+        
+    # return model , losses, 
+    return(glove, losses) 
+        
+    
+    
